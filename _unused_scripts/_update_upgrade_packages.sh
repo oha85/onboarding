474d1f7f92d1ccad7de899a2a5ce19fe9752870f
HANI <oualaa@hotmail.com>
2026-02-02 21:06:35 +0100

    Continue reorganzation 2

--- _unused_scripts/_update_upgrade_packages.sh
+++
@@ -1,8 +0,0 @@
-echo "Package update and upgrade starts. Please enter sudo password."
-read -p "Please confirm by pushing [Y] button to start updating and upgrading packages before  " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-   sudo apt autoremove && sudo apt autoclean
-   sudo apt update && sudo apt upgrade
-fi
-
