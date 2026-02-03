a8bf3c481874b7331e531d1f8729702de5f82779
HANI <oualaa@hotmail.com>
2026-02-03 14:13:35 +0100

    Continue implementing unzip functionality

--- core/03_unzip_softwares.sh
+++ core/03_unzip_softwares.sh
@@ -3,8 +3,27 @@ if [ -z $HOME_LAB/downloads ]; then
     echo $HOME_LAB"/downloads doesn't exist";
     return;
 fi
-while IFS= read -r -d '' file; do
-    echo "$file"
-done < <(find "$HOME_LAB/downloads" -type f -print0)
+files=$(find $HOME_LAB/downloads/ -type f -name "*.tar.gz" -printf '%f\n')
+for file in $files; do
+  case $file in
+    OpenJDK*)
+      echo OpenJDK
+    ;;
+   apache-maven*)
+      echo apacha-maven
+    ;;
+    *)
+    echo MIAOU
+    ;;
+  esac
+done
+
+
+
+#while IFS= read -r -d '' file; do
+ #   echo "$file"
+#done < <(find "$HOME_LAB/downloads" -type f -print0)
+
+#find $HOME_LAB/downloads/ -type f -name "*.tar.gz" ! -print
 
 #echo tar -xzf file.tar.gz
