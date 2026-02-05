831c1cae507a2a3ffb5e3f71a6e7610766a1aa29
HANI <oualaa@hotmail.com>
2026-02-05 21:54:44 +0100

    Adding apache-maven-4 url

---
+++ core/maven/download/4.0.0-rc-5.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+url=https://dlcdn.apache.org/maven/maven-4/4.0.0-rc-5/binaries/apache-maven-4.0.0-rc-5-bin.tar.gz
+core/_download.sh $url
--- core/maven/download/run.sh
+++ core/maven/download/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 core/maven/download/3-9-12.sh
+core/maven/download/4.0.0-rc-5.sh
