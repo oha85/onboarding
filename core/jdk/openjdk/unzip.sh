69371dc19e77d6f3bae2b41794412bbbf801af15
HANI <oualaa@hotmail.com>
2026-02-05 18:46:57 +0100

    Removing mv instruction ater unziping

--- core/jdk/openjdk/unzip.sh
+++ core/jdk/openjdk/unzip.sh
@@ -20,10 +20,4 @@ file=$1
 if [[ "$(isPathExists "$OPENJDK_DIR" eq "true" )" ]]; then
   jdk_version=$(getOpenJdkVersion $file)
   tar -xzvf "$DOWNLOADS_DIR/$file" -C $OPENJDK_DIR
-  if [[ "$(isPathNotExists "$UNZIPPED_DIR" eq "true" )" ]]; then
-    mkdir $UNZIPPED_DIR
-    echo "$UNZIPPED_DIR created."
-  fi
-  mv "$OPENJDK_DIR/$jdk_version" "$UNZIPPED_DIR"
-  echo "$OPENJDK_DIR/$jdk_version moved to $UNZIPPED_DIR"
 fi
